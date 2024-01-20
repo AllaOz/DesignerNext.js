9e6ce70573c0afda19fe5210ff4889266547ac68
AllaOz <ozcanalla@gmail.com>
2024-01-20 13:33:29 +0300

    header fixed

--- interior-design/src/app/components/Header/Header.jsx
+++ interior-design/src/app/components/Header/Header.jsx
@@ -7,49 +7,41 @@ import Image from 'next/image';
 function Header() {
   const [nav, setNav] = useState(false);
 
-  const handleToggleNav = () => {
-    setNav(!nav);
-  };
 
   return (
     <header className={styles.cardHeader}>
-      <nav className={`${styles.nav} ${nav ? styles.active : ''}`}>
+      
+        <nav className={styles.nav}>
         <Link href='/'>
             <Image src="/logo.jpg" className={styles.logo} width={70} height={70} alt="logo" />
         </Link>
-        <ul className={styles.menu}>
+        <ul className={
+          nav ? [styles.menu, styles.active].join(' ') : [styles.menu]}>
+         
           <li>
-            <Link href='/' onClick={handleToggleNav}>
-              Home
-            </Link>
+            <Link href="/" onClick ={()=> setNav(!nav)}>Home</Link>
           </li>
           <li>
-            <Link href='/notfound' onClick={handleToggleNav}>
-              Projects
-            </Link>
+            <Link href="/notfound" onClick ={()=> setNav(!nav)}>Projects</Link>
           </li>
           <li>
-            <Link href='/services' onClick={handleToggleNav}>
-              Services
-            </Link>
+            <Link href="/services" onClick ={()=> setNav(!nav)}>Services</Link>
           </li>
           <li>
-            <Link href='/aboutme' onClick={handleToggleNav}>
-              About me
-            </Link>
+            <Link href="/aboutme" onClick ={()=> setNav(!nav)}>About me</Link>
           </li>
           <li>
-            <Link href='/notfound' onClick={handleToggleNav}>
-              Contacts
-            </Link>
+            <Link href="/notfound"  onClick ={()=> setNav(!nav)}>Contacts</Link>
           </li>
+          
           <select className={styles.languagesSelect}>
             <option value="English">EN</option>
             <option value="Russian">RU</option>
           </select>
+         
         </ul>
-        <div onClick={handleToggleNav} className={styles.hamburgerMenu}>
-          {nav ? <AiOutlineClose size={25} /> : <AiOutlineMenu size={30} />}
+        <div onClick ={()=> setNav(!nav)} className={styles.hamburgerMenu}> {nav ? <AiOutlineClose size={25}/> :  <AiOutlineMenu size={30} />}
+       
         </div>
       </nav>
     </header>
