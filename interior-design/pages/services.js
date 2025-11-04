dd3ff34173b73b248da4c3da9638ba635fb6356c
AllaOz <ozcanalla@gmail.com>
2025-11-04 12:33:23 +0300

    Update free consultation button to navigate to /contacts page

--- interior-design/pages/services.js
+++ interior-design/pages/services.js
@@ -48,13 +48,13 @@ const Card = () => {
       <div className={styles.wrapperCard}>
         <div className={styles.paragraph_container}>
           <h2 className={styles.headingTwo}>Interior Design Services</h2>
-          <p className={styles.paragraphAboutme}>Transform your space with elegance and innovation! I’m offering a free consultation to bring your vision to life. Contact me now to take the first step toward creating a space that reflects your unique style and personality.
+          <p className={styles.paragraphAboutme}>Transform your space with elegance and innovation! I'm offering a free consultation to bring your vision to life. Contact me now to take the first step toward creating a space that reflects your unique style and personality.
           </p>
-          <a href="#contacts">
+          <Link href="/contacts">
             <button className={styles.freeConsultationBtn}>
               free consultation
             </button>
-          </a>
+          </Link>
         </div>
 
         <div className={styles.card_container} >
